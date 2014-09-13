a8aeb2645a8a03ac087f34b86046486092615190
Eric Smith <ericmsmith@gmail.com>
2014-09-12 21:14:41 -0400

    Specifying features and feature names

--- read_many_files.py
+++ read_many_files.py
@@ -12,6 +12,11 @@ Sources:
     - Data from S0101: AGE AND SEX
     - 2012 ACS 5-year estimates
     - Downloaded 2014-05-04
+- 2008_to_2012_race_and_ethnicity
+    - Searched the US Census' FactFinder with Geographies: {'County': ['All Counties within United States', 'All Counties within United States and Puerto Rico']}
+    - Data from B03002: HISPANIC OR LATINO ORIGIN BY RACE
+    - 2012 ACS 5-year estimates
+    - Downloaded 2014-05-04
 - 2008_to_2012_social_characteristics
     - Searched the US Census' FactFinder with Geographies: {'County': ['All Counties within United States', 'All Counties within United States and Puerto Rico']}
     - Data from DP02: SELECTED SOCIAL CHARACTERISTICS IN THE UNITED STATES
@@ -53,8 +58,33 @@ def main(con, cur):
 
     # Fields to extract: first level indicates folder, second level indicates
     # file name, third level indicates fields
-    fieldsD = {'2008_to_2012_age_and_sex': {},
-               '2008_to_2012_social_characteristics': {},
+    fieldsD = {'2008_to_2012_age_and_sex': 
+               {'ACS_12_5YR_S0101_with_ann.csv':
+                {'delimiter': ',', 'lines_to_ignore': 2,
+                 'fields': {178: 'median_age',
+                            184: 'sex_ratio'}}},
+               '2008_to_2012_race_and_ethnicity':
+               {'ACS_12_5YR_B03002_with_ann.csv':
+                {'delimiter': ',', 'lines_to_ignore': 2,
+                 'fields': {4: '2008_to_2012_race_and_ethnicity__total',
+                            8: 'white_not_hispanic__number',
+                            10: 'black_not_hispanic__number',
+                            14: 'asian_not_hispanic__number',
+                            26: 'hispanic_number'}}},
+               '2008_to_2012_social_characteristics':
+               {'ACS_12_5YR_DP02_with_ann.csv':
+                {'delimiter': ',', 'lines_to_ignore': 2,
+                 'fields': {54: 'households_with_children',
+                            58: 'households_with_senior_citizens',
+                            60: 'average_household_size',
+                            102: 'never_married',
+                            118: 'divorced',
+                            156: 'fertility',
+                            266: 'high_school_graduate',
+                            278: 'veterans',
+                            318: 'same_house_1_yr_ago',
+                            370: 'foreign_born',
+                            450: 'language_other_than_english_spoken_at_home'}}},
                '2010_to_2013_population': {},
                '2012_income_and_poverty': {},
                '2013_area': {},
