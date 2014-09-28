e48be9b6a34a09fb49bd61361c1ad15faf3ba879
EricMichaelSmith <ericmsmith@gmail.com>
2014-09-28 16:17:40 -0400

    Feature selection on multiple linear regression

--- confounding_factors.py
+++ confounding_factors.py
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from scipy import stats
+from sklearn import feature_selection, linear_model
 
 import config
 reload(config)
@@ -39,13 +40,25 @@ def main(con, cur):
     
     # Load feature data
     feature_s_l = config.feature_s_l
-    feature_d = selecting.select_fields(con, cur, feature_s_l, output_type='dictionary')
+    feature_d = selecting.select_fields(con, cur, feature_s_l, output_type='dictionary')    
+    
+    # Find and plot r-value of each feature with dem_fraction_shift
+#    pearsons_r_analysis(con, cur, feature_d, explanatory_d)  
+    
+    # Run recursive feature elimination with cross-validation
+    recursive_feature_elimination(con, cur, feature_d, explanatory_d)
+    
+            
+            
+def pearsons_r_analysis(con, cur, feature_d, explanatory_d):
+    """ Find and plot r-value of each feature (in feature_d) with dem_fraction_shift (in explanatory_d) """
     
     # Run linear regression on each feature separately
     r_value_d = {}
     r_value_5th_percentile_d = {}
     r_value_50th_percentile_d = {}
     r_value_95th_percentile_d = {}
+    explanatory_s = explanatory_d.keys()[0]
     for key_s in feature_d:
         is_none_b_a = np.equal(feature_d[key_s], None)
         feature1_a = np.array(feature_d[key_s])[~is_none_b_a]
@@ -87,6 +100,11 @@ def main(con, cur):
     # Plot the point and error bar, as well as guide lines
     ax.plot([0, 0], [0, num_features+1], c=(1, 0, 0))
     for l_feature, feature_s in enumerate(feature_by_r_value_l):
+        
+        # For unemployment_fraction_shift, highlight region in yellow
+        if feature_s == 'unemployment_fraction_shift':
+            plt.axhspan(l_feature+0.5, l_feature+1.5, facecolor=(1, 1, 0), alpha=0.5)
+        
         ax.plot([-1, 1], [l_feature+0.5, l_feature+0.5], c=(0.75, 0.75, 0.75))
         ax.scatter([r_value_d[feature_s]], [l_feature+1], c=(0,0,0))
         width_to_left = r_value_d[feature_s] - r_value_5th_percentile_d[feature_s]
@@ -96,40 +114,63 @@ def main(con, cur):
                     ecolor=(0,0,0))
                     
     # Configure axes
-    ax.set_position([0.45, 0.05, 0.53, 0.93])
+    ax.set_position([0.43, 0.05, 0.55, 0.93])
     ax.set_xlim([-1, 1])
     ax.set_xticks(np.arange(-1, 1.25, 0.25).tolist())
-    ax.set_xlabel("""Pearson's r between Obama vote shift and feature""")
+    ax.set_xlabel("""Pearson's r between feature and Obama vote shift""")
     ax.set_ylim([0.5, num_features+0.5])
     ax.set_yticks(np.arange(1, num_features+1, 1).tolist())
     ax.set_yticklabels(feature_by_r_value_l)
-  
-#    fig, axes = plt.subplots(nrows=len(feature_by_r_value_l),
-#                             figsize=(12, 1*len(feature_by_r_value_l)))
-##    fig.subplots_adjust(top=0.98, bottom=0.20, left=0.3, right=0.98)
-#    text_pos_x = -1.75
-#    text_pos_y = 0.00
-#    for ax, feature_s in zip(axes, feature_by_r_value_l[::-1]):
-#        ax.set_axis_bgcolor((1.0, 0.9, 0.9))
-#        ax.scatter([r_value_d[feature_s]], [0], c=(0,0,0))
-#        width_to_left = r_value_d[feature_s] - r_value_5th_percentile_d[feature_s]
-#        width_to_right = r_value_95th_percentile_d[feature_s] - r_value_d[feature_s]
-#        ax.errorbar([r_value_d[feature_s]], [0],
-#                    xerr=np.array([[width_to_left], [width_to_right]]))
-#        ax.set_xlim([-1, 1])
-#        ax.set_ylim([-1, 1])
-#        ax.xaxis.set_ticks(np.arange(-1, 1.25, 0.25).tolist())
-#        ax.yaxis.set_ticks([])
-#        for spine in ax.spines.itervalues():
-#            spine.set_visible(False)
-#        ax.text(text_pos_x, text_pos_y, feature_s, va='center', ha='left')
+    
+    # Show and save plot
     plt.show()
     plt.savefig(os.path.join(config.output_path_s, 'feature_r_values.png'))
-            
-            
-            
+
+
+
+def recursive_feature_elimination(con, cur, feature_d, explanatory_d):
+    """ Run recursive feature elimination with cross-validation {{{write this}}}. Template from http://scikit-learn.org/stable/auto_examples/plot_rfe_with_cross_validation.html. """
+    
+    # Transform feature dataset
+    feature_s_l = feature_d.keys()
+    X = np.array(feature_d[feature_s_l[0]], ndmin=2).T
+    ordered_feature_s_l = [feature_s_l[0]]
+    for feature_s in feature_s_l[1:]:
+        X = np.concatenate((X, np.array(feature_d[feature_s], ndmin=2).T), axis=1)
+        ordered_feature_s_l.append(feature_s)
+        
+    # Transform output dataset
+    explanatory_s = explanatory_d.keys()[0]
+    y = np.array(explanatory_d[explanatory_s])
+    
+    # Select only rows without Nones
+    is_none_b_a = np.equal(X, None)
+    no_none_features_b_a = (np.sum(is_none_b_a, axis=1) == 0)
+    X = X[no_none_features_b_a,]
+    y = y[no_none_features_b_a]
+    
+    # Create recursive feature elimination and fit to data
+    regr = linear_model.LinearRegression()
+    rfecv = feature_selection.RFECV(estimator=regr, step=1)
+    rfecv.fit(X, y)
+    print('Optimal number of features: %d' % rfecv.n_features_)
+    i_selected_features_a = rfecv.get_support(indices=True)
+    for i in i_selected_features_a:
+        print('Selected feature: %s' % ordered_feature_s_l[i])
+    
+    # Plot number of features VS. cross-validation scores
+    plt.figure()
+    plt.xlabel("Number of features selected")
+    plt.ylabel("Cross validation score (nb of correct classifications)")
+    plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+    plt.show()
+    
+    # {{{figure out which features are most useful, by how much, and plot this in the best way}}}
+    
+
+
 def regression_confidence_interval_wrapper(index_l, con, cur, feature1_a, feature2_a):
-    """ {{{write this!!!}}} """
+    """ Allows for bootstrapping over samples in feature1_a and feature2_a, given index of samples index_l """
     
     slope, intercept, r_value, p_value, std_err = \
         stats.linregress(feature1_a[index_l].tolist(), feature2_a[index_l].tolist())
--- main.py
+++ main.py
@@ -16,10 +16,13 @@ s: string
 t: tuple
 Underscores indicate chaining: for instance, "foo_t_t" is a tuple of tuples
 
-2014-09-27:
-- What are you going to do about the racial correlations that will inevitably come out of this? Maybe just remove the violent crime feature?
-- If you think it's best, see your bookmark folder "2014-09-19 Multiple linear regression with cross-validation" for how to automatically implement CV-based feature selection on your feature set. Then, find some way to rank the best features for a regression model with n features. See how the ranking of the magnitude of the p-values or r-values lines up with the coefficients of the normalized features in the multiple linear regression: will this tell you something about the covariance of the features?
--When you're done with all of that, see the OneNote page task list for other stuff to do.
+2014-09-28:
+- Feature selection results: white_not_hispanic_fraction, unemployment_fraction_shift, black_not_hispanic_fraction, asian_not_hispanic_fraction, obese, percent_non_senior_citizens_without_insurance, population_change_fraction
+- Figure out what the feature-selection results mean. Maybe find some way to rank the best features for a regression model with n features? See how the ranking of the magnitude of the p-values or r-values lines up with the coefficients of the normalized features in the multiple linear regression: will this tell you something about the covariance of the features?
+- Run lasso and ridge regression on your data, right?
+- In article, find something that backs up your finding that whites left Obama in droves (call it the Matt Damon effect? Or just be very careful about this...)
+- Before posting, search the code for "{{{" and "[[[" and "((("
+- When you're done with all of that, see the OneNote page task list for other stuff to do.
 """
 
 import MySQLdb
